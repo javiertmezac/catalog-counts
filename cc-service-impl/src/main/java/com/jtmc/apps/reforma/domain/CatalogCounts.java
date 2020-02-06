9742d0d59f4521a83ed6dc933aa10523c8455e20
Meza, Javier <javier.meza@thermofisher.com>
2020-02-05 20:57:04 -0800

    added class to store in Json File
    - added some junit test to catalog counts class

--- cc-service-impl/src/main/java/com/jtmc/apps/reforma/domain/CatalogCounts.java
+++ cc-service-impl/src/main/java/com/jtmc/apps/reforma/domain/CatalogCounts.java
@@ -1,5 +1,7 @@
 package com.jtmc.apps.reforma.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.time.LocalDateTime;
 
 public class CatalogCounts {
@@ -17,7 +19,10 @@ public class CatalogCounts {
         return amount;
     }
 
-    public void setAmount(double amount) {
+    public void setAmount(double amount) throws Exception {
+        if (amount <= 0.0) {
+            throw new Exception("Amount cannot be less or equals zero");
+        }
         this.amount = amount;
     }
 
@@ -25,7 +30,11 @@ public class CatalogCounts {
         return countsEnum;
     }
 
-    public void setCountsEnum(CatalogCountsEnum countsEnum) {
+    public void setCountsEnum(CatalogCountsEnum countsEnum) throws Exception {
+        if ( countsEnum == null) {
+            throw new Exception("counts cannot be null");
+        }
+
         this.countsEnum = countsEnum;
     }
 
@@ -33,7 +42,11 @@ public class CatalogCounts {
         return details;
     }
 
-    public void setDetails(String details) {
+    public void setDetails(String details) throws Exception {
+        if (StringUtils.isBlank(details)) {
+            throw new Exception("Details should not be blank");
+        }
+
         this.details = details;
     }
 
@@ -41,7 +54,10 @@ public class CatalogCounts {
         return registrationDateTime;
     }
 
-    public void setRegistrationDateTime(LocalDateTime registrationDateTime) {
+    public void setRegistrationDateTime(LocalDateTime registrationDateTime) throws Exception {
+        if (registrationDateTime == null) {
+            throw new Exception("registration DateTime shouldn't be null");
+        }
         this.registrationDateTime = registrationDateTime;
     }
 }
