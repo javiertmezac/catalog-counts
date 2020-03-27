bf01a57cf67c3a7ebc246979a3a8523ada79fdbc
Meza, Javier <javiermeza.cazarez@gmail.com>
2020-03-26 20:58:32 -0700

    Flyway with migrations configuration     - removed cc-service-db module     - added flyway-db-configuration folder     - flyway migration to insert catalog count enum

---
+++ flyway-db-configuration/flyway-3.2.1/sql/V1.1.0__catalog_count_enum_insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO catalog_count_enum
+(
+  identifier,
+  name,
+  description,
+  TYPE,
+  isDeleted
+)
+VALUES
+(
+  '1.2',
+  'Diezmo',
+  'por parte de los hermanos o actividades de la iglesia',
+  TRUE,
+  TRUE
+);
