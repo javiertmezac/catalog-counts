188d6ea19d7a52bbd02915a6badd84d9c4180504
javiertmezac <javiermeza.cazarez@gmail.com>
2020-10-26 21:12:46 -0700

    manually deleted all excel import functionality, this is covered in another branch

--- cc-service-impl/src/main/java/com/jtmc/apps/reforma/service/excelimport/ExcelImportServiceSheetNotValidException.java
+++
@@ -1,8 +0,0 @@
-package com.jtmc.apps.reforma.service.excelimport;
-
-public class ExcelImportServiceSheetNotValidException extends Exception {
-
-    public ExcelImportServiceSheetNotValidException(String message) {
-        super(message);
-    }
-}
