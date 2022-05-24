9ae381df3d40018da5afc71c2eb3366be76afb7f
javiertmezac <javiermeza.cazarez@gmail.com>
2022-05-23 23:33:01 -0700

    remove extra comma

--- flyway-db-configuration/flyway-6.5.5/sql/V0.0.10__adding_report.sql
+++ flyway-db-configuration/flyway-6.5.5/sql/V0.0.10__adding_report.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `report` (
   `status`          BOOLEAN NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `report_reportedBy_FK` FOREIGN KEY (`reportedBy`) REFERENCES `catalog_count`.`persona` (`id`),
-  CONSTRAINT `report_periodDetails_FK` FOREIGN KEY (`periodDetailsId`) REFERENCES `catalog_count`.`period_details` (`id`),
+  CONSTRAINT `report_periodDetails_FK` FOREIGN KEY (`periodDetailsId`) REFERENCES `catalog_count`.`period_details` (`id`)
 )
   DEFAULT CHARSET =utf8
   COLLATE =utf8_unicode_ci;
