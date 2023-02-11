71d7d433126c2745c02671197d35333e9957b3a7
JavierTMezaC <javiermeza.cazarez@gmail.com>
2023-02-11 00:27:17 -0800

    add initial amount for each branch as catalog_count_enum
    update readme

--- local-provision.sh
+++ local-provision.sh
@@ -2,9 +2,9 @@
 clear
 
 DEFAULT_DB_PASS="Password1"
-DEFAULT_CC_SERVICE_KEY="some_random_value_here"
+DEFAULT_CC_SERVICE_KEY="some_random_value_here_what_else_can_I_use"
 export MYSQL_ROOT_PASSWORD=$DEFAULT_DB_PASS
-expoert CC_SERVICE_KEY=$DEFAULT_CC_SERVICE_KEY
+export CC_SERVICE_KEY=$DEFAULT_CC_SERVICE_KEY
 
 
 printf "[INFO] Packaging application\n"
