70565870e616873bbc4f9215d6e44062f3abc363
Javier T Meza C <javiermeza.cazarez@gmail.com>
2024-03-10 21:55:19 -0700

    add 4.5 ahorro para conferencias - catalog_count - 2.1.0.2 (#75)
    * add 4.5 ahorro para conferencias - catalog_count
    
    * add 4.5 ahorro..
    
    add family
    
    minor changes in code

---
+++ flyway-db-configuration/flyway-6.5.5/sql/V0.0.12__adding_conference_saving_cc.sql
@@ -0,0 +1,18 @@
+INSERT INTO catalog_count_enum
+(
+  identifier,
+  family,
+  name,
+  description,
+  type,
+  isDeleted
+)
+VALUES
+(
+  '4.5',
+  'general',
+  'Ahorro para Conferencias',
+  'Ahorro del 5% de las ofrendas semanales',
+  TRUE,
+  FALSE
+);
