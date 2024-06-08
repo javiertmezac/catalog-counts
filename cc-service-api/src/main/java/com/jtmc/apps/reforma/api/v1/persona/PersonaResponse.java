7b3b6430c3c56ae89ddc872b1d64fb15084d7c02
javiertmezac <javiermeza.cazarez@gmail.com>
2024-06-08 13:56:40 -0700

    persona API - CRUD

--- cc-service-api/src/main/java/com/jtmc/apps/reforma/api/v1/persona/PersonaResponse.java
+++ cc-service-api/src/main/java/com/jtmc/apps/reforma/api/v1/persona/PersonaResponse.java
@@ -1,9 +1,29 @@
 package com.jtmc.apps.reforma.api.v1.persona;
 
+import java.time.Instant;
+
 public class PersonaResponse {
 
     private int id;
     private String completeName;
+    private String registration;
+    private  boolean status;
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 
     public int getId() {
         return id;
